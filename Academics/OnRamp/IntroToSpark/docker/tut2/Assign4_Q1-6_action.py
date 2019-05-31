c209937d035e7b5fd00842406eb24076eb47f299
Tyler Peterson <petersontylerd@gmail.com>
2019-05-31 17:57:07 -0500

    docker and spark work
    Former-commit-id: 19092c1a8fc8773d8831bab8121815b4c3ba0dff
    
    Former-commit-id: 6f4143ac82e61eb193ce4758871cdd58eee3466c
    Former-commit-id: db05bd339ea9776efaf9e42cd3fd1154a694c4d1

--- Academics/OnRamp/IntroToSpark/docker/tut2/Assign4_Q1-6_action.py
+++ Academics/OnRamp/IntroToSpark/docker/tut2/Assign4_Q1-6_action.py
@@ -13,52 +13,44 @@ df = spark.read.format('csv').option('header','true').load(path)
 # cast income as an integer
 df = df.withColumn('Income', df['Income'].cast(IntegerType())) 
 
-
 # Question 1
 print('*' * 30)
 print('\nQuestion 1\n')
-
-
-sqlDf.show()
+print(df.rdd.map(lambda x: (x[1], x[0])).groupByKey().mapValues(lambda vals: len(set(vals))).sortBy(lambda a: a[1], ascending = False).take(1))
 print('\n\n')
 
 
 # Question 2
 print('*' * 30)
 print('\nQuestion 2\n')
-
-sqlDf.show()
+print(df.rdd.filter(lambda v: v[1] == 'United States of America').map(lambda x: (x[1], x[4])).groupByKey().mapValues(lambda x: sum(x) / len(x)).collect())
 print('\n\n')
 
 
 # Question 3
 print('*' * 30)
 print('\nQuestion 3\n')
-
-sqlDf.show()
+print(df.rdd.filter(lambda v: v[4] > 100000).filter(lambda v: v[7] == 'FALSE').count())
 print('\n\n')
 
 
 # Question 4
 print('*' * 30)
 print('\nQuestion 4\n')
-
-sqlDf.show()
+print(df.rdd.filter(lambda v: v[1] == 'United States of America').sortBy(lambda x: x[4], ascending = False).map(lambda x: (x[3], x[6], x[4], x[5])).take(10))
 print('\n\n')
 
 
 # Question 5
 print('*' * 30)
 print('\nQuestion 5\n')
-
-sqlDf.show()
+print(df.rdd.groupBy(lambda x: x[5]).count())
 print('\n\n')
 
 
 # Question 6
 print('*' * 30)
 print('\nQuestion 6\n')
-
-sqlDf.show()
+print(df.rdd.filter(lambda v: v[5] == 'Writer').filter(lambda x: x[4] < 100000).count())
 print('\n\n')
 
